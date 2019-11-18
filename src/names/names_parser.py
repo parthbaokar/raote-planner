e88b6335f0afa67cbb9f94c25ebf3f18d4ddcae8
Parth Baokar <parthbaokar@berkeley.edu>
2019-11-18 00:47:13 -0800

    Created master list of city names and functionality to choose and clean the list of master names

---
+++ src/names/names_parser.py
@@ -0,0 +1,29 @@
+import re
+import random
+
+def clean_names():
+    with open('master_names.txt', 'r') as file:
+        name_input = file.readlines()
+    name_input = [line.rstrip() for line in name_input]
+    name_output = set()
+    for full_name in name_input:
+        if (len(full_name.split()) >= 2):
+            continue
+        if re.match('^[\w]+$', full_name) and len(full_name) <= 20:
+            name_output.add(full_name)
+    open('master_names.txt', 'w').close()
+    with open('master_names.txt', 'w') as file:
+        for s in name_output:
+            file.write("%s\n" % s)
+
+
+def choose_cities(number):
+    input_file = open('master_names.txt', 'r')
+
+    name_input = input_file.readlines()
+    name_input = [line.rstrip() for line in name_input]
+    if number < len(name_input):
+        choice = random.sample(name_input, k=number)
+        return ' '.join(choice)
+    else:
+        raise ValueError('Must choose a number of cities less than the size of the master list of cities.')
