ff7d35575958e651f679d2bdc2f8f8ed30987113
Albert Tang <alberttang@berkeley.edu>
2019-11-21 19:54:14 -0800

    needs work on dropoffs, handing off to parth @ soda

--- src/io_generators/output_generator.py
+++ src/io_generators/output_generator.py
@@ -6,18 +6,44 @@ import sys
 
 def genin(cities, numcities, homes, numhomes, start):
     filenamein = str(numcities) + ".in"
-    filein = open(filenamein, "w")
-    filenamein.write(allcities)
-    filenameout.write(' '.join(cycle))
+    with open(filenamein, "w+") as file:
+        file.write(numcities)
+        file.write(numhomes)
+        file.write(cities)
+        file.write(homes)
+        file.write(start)
 
-def genout(numcities, numhomes, lencycle):
-    filenameout = str(i) + ".out"
-    fileout = open(filenameout, "w")
+        #Graph
 
-    allcities = choose_cities(numcities)
+
+
+def genout(numcities, numhomes, lencycle, numdropoffs):
+    filenameout = str(numcities) + ".out"
+    allcities = choose_cities(nties.split(), k=numcities)
     cycle = random.sample(allcities.split(), k=lencycle)
+    homes = random.sample(allcities.split(), k=numhomes)
+    cyclecopy = list(cycle)
+    random.shuffle(cyclecopy)
+    dropoffstrings = []
+    
+    for i in range(numdropoffs):
+        dropoff = cyclecopy[i]
+        out = dropoff
+        if dropoff in homes:
+            out += " " + dropoff
+
+
+
+    with open(filenameout, "w+") as file:
+        file.write(' '.join(cycle + [cycle[0]]))
+
+
 
 
 if __name__ == '__main__':
     for i in [40, 100, 169]:
-        
+        print(i)
+        numhomes = int(input("Number of homes: "))
+        lencycle = int(input("Length of cycle: "))
+        numdropoffs = int(input("How many dropoffs: "))
+        genout(i, numhomes, lencycle, numdropoffs)
